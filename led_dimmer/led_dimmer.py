9e6863c5edc6d832ec5bc69dbce63de4666966f8
swalzel1 <sterling_walzel1@baylor.edu>
2024-10-31 12:53:08 -0500

    h

--- led_dimmer/led_dimmer.py
+++ led_dimmer/led_dimmer.py
@@ -5,7 +5,6 @@ import os
 
 sys.path.insert(0, '../utilities')
 import utilities
-
 pwm = utilities.HW_PWM(2000)
 
 try:
@@ -15,11 +14,11 @@ try:
         pwm.set_duty_cycle(pwm_value)
         pwm_value = pwm_value + 0.5*factor
         if pwm_value < 0.0:
-        pwm_value = 0.0
-        factor = 1
+            pwm_value = 0.0
+            factor = 1
         if pwm_value > 100.0:
-        pwm_value = 100.0
-        factor = -1
+            pwm_value = 100.0
+            factor = -1
         time.sleep(.01)
 except KeyboardInterrupt:
 print('Got Keyboard Interrupt. Cleaning up and exiting')
