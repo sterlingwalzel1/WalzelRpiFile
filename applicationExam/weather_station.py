32557ff7861a3a0f6ad62a3f4f808052964e6b68
swalzel <sterling_walzel1@baylor.edu>
2024-11-19 12:36:39 -0600

    a

--- applicationExam/weather_station.py
+++ applicationExam/weather_station.py
@@ -23,12 +23,53 @@ try:
     pwm_value = 0.0
     factor = 1
     while True:
+        #getting temp value
+        to_send = [0b00000001, 0b00100000, 0b00000000]
+        value = spi.xfer(to_send)
+        temp = (value[1] * 256) + value[2]
+        temp = (temp /1024) * 3.3 #convert numerical value to voltage
+        temp = (temp - 0.5) / 0.01 #convert voltage to temp c
+        temp = (1.8 * temp) + 32 #convert to c to f
+
+        #getting photo sensor value
         to_send = [0b00000001, 0b10000000, 0b00000000]
         value = spi.xfer(to_send)
         adc_value = (value[1] * 256) + value[2]
-        print(adc_value)
-        pwm_value = (min_val + adc_value) / ((max_val-min_val)/100)
+        photo_volt = 3.3 - ((adc_value /1024) * 3.3) #convert numerical value to voltage
+
+        #figure out what activity
+        if temp < 35.0:
+            activity = 'STAY_HOME'
+        elif temp > 35.0 and temp < 50.0:
+            activity = 'BONFIRE'
+        elif temp > 50.0 and temp < 65.0:
+            if photo_volt > 2.0:
+                activity = 'SURFS_UP'
+            else:
+                activity = 'TOO_DARK_TO_SURF'
+        else:
+            if photo_volt > 2.0:
+                activity = 'BAG_RAYS'
+            else:
+                activity = 'TOO_DARK_TO_BAG_RAYS'
+
+        #SWITCH CASE TO FIGURE OUT WHAT ACTIVITY
+        if activity == "STAY_HOME":
+            print('Stay home, dude!')
+        elif activity == 'BONFIRE':
+            print('Bonfire, dude!')
+        elif activity == 'SURFS_UP':
+            print('Surf\'s up, dude!')
+        elif activity == 'TOO_DARK_TO_SURF':
+            print('Too dark to surf, dude!')
+        elif activity == 'BAG_RAYS':
+            print('Bag some rays, dude!')
+        elif activity == 'TOO_DARK_TO_BAG_RAYS':
+            print('Too dark to bag Rays, dude!')
+
+        print(temp, photo_volt)
         time.sleep(1)
+  
   
 except KeyboardInterrupt:
     print('Got Keyboard Interrupt. Cleaning up and exiting')
